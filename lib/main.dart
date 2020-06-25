188bea4a787ad4cea7e28cb76317312a99ec15f2
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-06-24 23:22:29 -0400

    Creating home_screen and started implementing skeleton ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:posture_app/screens/home_screen.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarBrightness: Brightness.light,
+  ));
   runApp(MyApp());
 }
 
@@ -10,9 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        canvasColor: Colors.green[50],
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomeScreen(),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,8 +3,94 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Center(child: Text('Hello')),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            Flexible(
+              flex: 6,
+              child: Container(
+                // height: deviceHeight * 0.50,
+                child: Center(
+                  child: Text('Yo'),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.green[200],
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                DropdownButton(
+                  underline: Container(),
+                  icon: Icon(Icons.more_vert),
+                  items: [
+                    DropdownMenuItem(
+                      child: Row(
+                        children: [
+                          Icon(Icons.settings),
+                          SizedBox(width: 8),
+                          Text('Settings'),
+                        ],
+                      ),
+                      value: 'settings',
+                    ),
+                    DropdownMenuItem(
+                      child: Row(
+                        children: [
+                          Icon(Icons.tag_faces),
+                          SizedBox(width: 8),
+                          Text('About us'),
+                        ],
+                      ),
+                      value: 'about',
+                    ),
+                  ],
+                  onChanged: (itemIdentifier) {},
+                ),
+              ],
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            Flexible(
+              flex: 2,
+              child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.blue[100],
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(50),
+                  ),
+                ),
+                child: Icon(
+                  Icons.play_arrow,
+                  size: 40,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            Center(
+              child: Text(
+                'This is where a tip will go...',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
